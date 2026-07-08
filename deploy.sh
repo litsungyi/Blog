e0248bcc37087c4dc060932c26e2854950d48dc7
OLD_RD6李宗沂 <tsungyili@igs.com.tw>
2026-07-08 11:00:38 +0800

    feat: Update project

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
-# deploy.sh
+#!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo
+hugo --gc --minify
+
+# Go to public folder
+mkdir -p public
+cd public || exit
 
 # Add changes to git.
-git add -A
+git add .
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -14,6 +18,8 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master -f
-git subtree push --prefix=public git@github-litsungyi:litsungyi/Blog gh-pages
+# Push to remote
+git push origin gh-pages
+
+# Return to root
+cd ..
